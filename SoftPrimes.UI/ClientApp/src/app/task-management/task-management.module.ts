ecba9340a6003805cb1a7236a411fe409cb1bf2b
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-08-04 13:34:00 +0200

    task management module

---
+++ SoftPrimes.UI/ClientApp/src/app/task-management/task-management.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskManagementRoutingModule } from './task-management-routing.module';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskManagementWrapperComponent } from './task-management-wrapper/task-management-wrapper.component';
+
+
+@NgModule({
+  declarations: [TasksListComponent, NewTaskComponent, TaskManagementWrapperComponent],
+  imports: [
+    CommonModule,
+    TaskManagementRoutingModule
+  ]
+})
+export class TaskManagementModule { }
