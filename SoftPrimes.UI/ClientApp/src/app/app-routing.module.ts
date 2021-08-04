bdca8e5958f6603297dbc95ba44d86b878af358f
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-08-04 13:35:04 +0200

    app component modules and routing structure

---
+++ SoftPrimes.UI/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
+
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'task-management', loadChildren: './task-management/task-management.module#TaskManagementModule' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
