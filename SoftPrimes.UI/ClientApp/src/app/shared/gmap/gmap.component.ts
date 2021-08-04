867eecf13613b704aa3c81b799e61d580b51390b
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-08-04 19:22:46 +0200

    google map component

---
+++ SoftPrimes.UI/ClientApp/src/app/shared/gmap/gmap.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gmap',
+  templateUrl: './gmap.component.html',
+  styleUrls: ['./gmap.component.css']
+})
+export class GmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openInFullscreen(el) {
+     // if already full screen; exit
+    // else go fullscreen
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    } else {
+      const element = el;
+      console.log(element);
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    }
+  }
+
+}
