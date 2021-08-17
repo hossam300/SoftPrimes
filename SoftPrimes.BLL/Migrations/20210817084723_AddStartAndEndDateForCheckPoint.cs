fadb5fa6a50eb08f735c3795a71328d26cc83a8c
Hossam Fathy <hfathy@tafeel.com>
2021-08-17 10:47:39 +0200

    AddStartAndEndDateForCheckPoint

---
+++ SoftPrimes.BLL/Migrations/20210817084723_AddStartAndEndDateForCheckPoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoftPrimes.BLL.Migrations
+{
+    public partial class AddStartAndEndDateForCheckPoint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "TourCheckPoints",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "TourCheckPoints",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "TourCheckPoints");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "TourCheckPoints");
+        }
+    }
+}
